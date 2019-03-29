d497e196ba2a965711750584eb870756803c02fc
jmartin3 <jmartin3@iastate.edu>
2019-03-29 13:58:22 -0500

    Began Lab8

---
+++ Lab8/l8_main.c
@@ -0,0 +1,22 @@
+/**
+ * l8_main.c
+ *
+ * Lab 8:
+ *
+ * @author jmartin3, irex
+ * @date  Mar, 29, 2019
+ */
+#include "lcd.h"
+#include "servo.h"
+#include <inc/tm4c123gh6pm.h>
+
+
+int main(void)
+{
+    servo_init();
+
+    while(1){
+
+    }
+
+}
